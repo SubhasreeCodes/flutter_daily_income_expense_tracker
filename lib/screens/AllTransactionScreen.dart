779d0e8b4cd802e5b23f6ed138b4101e6bd5a396
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-01-11 18:37:15 +0530

    after update fetchTransactions AllTransactionScreen logic added

--- lib/screens/AllTransactionScreen.dart
+++ lib/screens/AllTransactionScreen.dart
@@ -5,6 +5,8 @@ import 'package:flutter_daily_income_expense_tracker/models/Transaction.dart';
 import 'package:flutter_daily_income_expense_tracker/utils/Constants.dart';
 import 'package:http/http.dart' as http;
 
+import 'TransactionFormScreen.dart';
+
 class AllTransactionScreen extends StatefulWidget {
 
   final String title;
@@ -92,6 +94,21 @@ class _AllTransactionScreenState extends State<AllTransactionScreen> {
                   ),
                   subtitle: Text(row.description),
                   trailing: Text(row.date),
+                  onTap: () async {
+                    // print(row.toJson());
+                    // Navigator.pop(context);
+                    await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => TransactionFormScreen(
+                          title: 'Update Transaction',
+                          transactionId: row.id.toString(),
+                          transactionData: row,
+                        ),
+                      ),
+                    );
+                    // Refresh the transactions list when the user returns
+                    fetchTransactions();
+                  },
                 ));
           }),
     );
